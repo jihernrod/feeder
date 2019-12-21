a469b971a9977a3e47e6a6f53bff564d981ad67a
jesus <jihernandezr@gmail.com>
2019-12-21 19:10:55 +0100

    firts commit

---
+++ src/test/java/matxu/framework/agregation/feeder/FeederApplicationTests.java
@@ -0,0 +1,13 @@
+package matxu.framework.agregation.feeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeederApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
