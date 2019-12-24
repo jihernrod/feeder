234e2c9c4b957e9bbaffd2298d1a49c61dd588ab
jesus <jihernandezr@gmail.com>
2019-12-24 10:15:02 +0100

    Added configuration object to the job

--- src/main/java/matxu/framework/agregation/feeder/config/ScenaryBatchConfiguration.java
+++ src/main/java/matxu/framework/agregation/feeder/config/ScenaryBatchConfiguration.java
@@ -4,25 +4,31 @@ import matxu.framework.agregation.feeder.processors.ScenaryItemProcessor;
 import matxu.framework.agregation.feeder.listener.JobCompletionNotificationListener;
 import matxu.framework.agregation.feeder.model.Scenary;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.data.MongoItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import java.util.Map;
+
 @Configuration
 @EnableBatchProcessing
 public class ScenaryBatchConfiguration {
 
+    public Map<String, JobParameter> NOT_SET =null;
 
     @Autowired
     private MongoTemplate mongoTemplate;
@@ -34,7 +40,8 @@ public class ScenaryBatchConfiguration {
     public StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public FlatFileItemReader<Scenary> reader() {
+    @StepScope
+    public FlatFileItemReader<Scenary> reader(@Value("#{jobParameters}")  Map<String, JobParameter> jobParameters) {
         return new FlatFileItemReaderBuilder<Scenary>()
                 .name("scenaryItemReader")
                 .resource(new ClassPathResource("scenary.csv"))
@@ -47,12 +54,14 @@ public class ScenaryBatchConfiguration {
     }
 
     @Bean
-    public ScenaryItemProcessor processor() {
+    @StepScope
+    public ScenaryItemProcessor processor(@Value("#{jobParameters}")  Map<String, JobParameter> jobParameters) {
         return new ScenaryItemProcessor();
     }
 
     @Bean
-    public MongoItemWriter<Scenary> writer() {
+    @StepScope
+    public MongoItemWriter<Scenary> writer(@Value("#{jobParameters}")  Map<String, JobParameter> jobParameters) {
         MongoItemWriter<Scenary> writer = new MongoItemWriter<Scenary>();
         writer.setTemplate(mongoTemplate);
         writer.setCollection("scenary");
@@ -75,9 +84,9 @@ public class ScenaryBatchConfiguration {
     public Step step1(MongoItemWriter<Scenary> writer) {
         return stepBuilderFactory.get("step1")
                 .<Scenary, Scenary> chunk(10)
-                .reader(reader())
-                .processor(processor())
-                .writer(writer)
+                .reader(reader(NOT_SET))
+                .processor(processor(NOT_SET))
+                .writer(writer(NOT_SET))
                 .build();
     }
     // end::jobstep[]
